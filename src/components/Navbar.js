5ee4ab875f4be8328d5b88d3265ec5427093f2c3
KaviArasu_NS <KaviArasu_NS@icloud.com>
2022-08-26 20:14:15 +0530

    barchart

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,8 +6,20 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
+import { useDispatch, useSelector } from "react-redux";
+import { logoutInitiate } from "../redux/actions";
+import { Avatar } from "@mui/material";
 
-export default function NavbarBar({ currentUser }) {
+export default function NavbarBar() {
+  const { currentUser } = useSelector((state) => state.user);
+  const dispatch = useDispatch();
+
+  const handleAuth = () => {
+    if (currentUser) {
+      dispatch(logoutInitiate());
+    }
+  };
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
@@ -24,8 +36,18 @@ export default function NavbarBar({ currentUser }) {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             News
           </Typography>
-          <img src={currentUser.photoURL} alt="" />
-          <Button color="inherit">{currentUser ? "LOGOUT" : "LOGIN"}</Button>
+          <Button color="inherit" style={{ fontWeight: "bold" }}>
+            {currentUser.displayName}
+          </Button>
+          <Avatar src={currentUser && currentUser.photoURL} />
+          {/* <img src={currentUser.photoURL} alt="" /> */}
+          <Button
+            color="inherit"
+            onClick={handleAuth}
+            style={{ fontWeight: "bold" }}
+          >
+            {currentUser ? "LOGOUT" : "LOGIN"}
+          </Button>
         </Toolbar>
       </AppBar>
     </Box>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,27 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import BarChart from "../components/BarChart";
 import Navbar from "../components/Navbar";
 import { logoutInitiate } from "../redux/actions";
+import { API } from "../global";
 
 const Home = () => {
-  const { currentUser } = useSelector((state) => state.user);
-  const dispatch = useDispatch();
+  const [debt, setDebt] = useState();
 
-  const handleAuth = () => {
-    if (currentUser) {
-      dispatch(logoutInitiate());
-    }
+  const getadaniDebt = () => {
+    fetch(`${API}/chartjs`, {
+      method: "GET",
+    })
+      .then((data) => data.json())
+      .then((debt) => {
+        setDebt(debt);
+      });
   };
+
+  useEffect(() => getadaniDebt(), []);
+
+  // const comp = debt.map((data) => data.companyName);
+  // console.log(comp);
+
+  const chartData = {
+    labels: debt ? debt.map((data) => data.companyName) : [],
+    datasets: [
+      {
+        label: "Adani Group's Company vise Debt Percent",
+        data: debt ? debt.map((data) => data.debtPercent) : [],
+        backgroundColor: debt ? debt.map((data) => data.color) : [],
+      },
+    ],
+  };
+
+  const { currentUser } = useSelector((state) => state.user);
 
   console.log(currentUser.displayName);
   return (
     <div>
-      <Navbar currentUser={currentUser} />
-      <h2>Welcome to Our Site {currentUser.displayName}</h2>
+      <Navbar />
       <br />
-      <button className="btn btn-danger" onClick={handleAuth}>
+      {/* <button className="btn btn-danger" onClick={handleAuth}>
         Logout
-      </button>
+      </button> */}
+      {debt ? <BarChart chartData={chartData} /> : "Loading..."}
     </div>
   );
 };
